1b469a7ff369820d29768782b8cf359ae382b462
Emmanuel Okyere <chief@hutspace.net>
2014-03-25 06:18:29 +0000

    Added has_liked and has_unliked utils and template tags

--- likes/static/likes/includes/likes.js
+++ likes/static/likes/includes/likes.js
@@ -1,20 +1,18 @@
-if (typeof $ != 'undefined'){
+if (typeof $ != 'undefined') {
+    $(function() {
 
-    $(document).ready(function(){
-
-        $(document).on('click', 'a.liker', function(event){
+        $(document).on('click', 'a.liker', function(event) {
             event.preventDefault();
-            var el = $(this);
-            var replace_selector = el.attr('replace_selector');
-            if (!replace_selector)
-                var replace_target = el.parents('.likes:first');
-            else
-                var replace_target = $(replace_selector);
-            $.get(el.attr('href'), {}, function(data){
-                replace_target.html(data);
+            $.get($(this).attr('href'), {}, function(data){
+                location.reload();
             });
         });
 
+        $(document).on('click', 'a.unliker', function(event) {
+            event.preventDefault();
+            $.get($(this).attr('href'), {}, function(data) {
+                location.reload();
+            });
+        });
     });
-
 }
